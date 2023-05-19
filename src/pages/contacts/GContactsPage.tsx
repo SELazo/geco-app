2d9ce584ccbac593a0738fd8620718fd492762b9
JAlexsse <jessica.altenburger@outlook.com>
2023-05-19 17:41:44 -0300

    fix: adding background and geco in contacts home page

--- src/pages/contacts/GContactsPage.tsx
+++ src/pages/contacts/GContactsPage.tsx
@@ -16,33 +16,31 @@ import { GLogoLetter } from '../../components/GLogoLetter';
 
 export const GContactsPage = () => {
   return (
-    <>
-      <div className="geco-contacts">
-        <div className="geco-contacts-head">
-          <div className="geco-contacts-head-nav-bar">
-            <Link className="geco-contacts-head-nav-bar-logo" to="/home">
-              <GLogoLetter />
-            </Link>
-            <GCircularButton
-              icon={GIconButtonBack}
-              size="1.5em"
-              width="50px"
-              height="50px"
-              colorBackground={GWhite}
-              onClickAction={NavigationService.goBack}
-            />
-          </div>
-          <div className="geco-pricing-title">
-            <GHeadCenterTitle title={ContactsSectionTitle} color={GBlack} />
-          </div>
+    <div className="geco-contacts">
+      <div className="geco-contacts-head">
+        <div className="geco-contacts-head-nav-bar">
+          <Link className="geco-contacts-head-nav-bar-logo" to="/home">
+            <GLogoLetter />
+          </Link>
+          <GCircularButton
+            icon={GIconButtonBack}
+            size="1.5em"
+            width="50px"
+            height="50px"
+            colorBackground={GWhite}
+            onClickAction={NavigationService.goBack}
+          />
         </div>
-        <div className="geco-contacts-body">
-          <div className="geco-contacts-options">
-            <GMenuOption option={AdminListContacts} />
-            <GMenuOption option={GroupsContacts} />
-          </div>
+        <div className="geco-pricing-title">
+          <GHeadCenterTitle title={ContactsSectionTitle} color={GBlack} />
         </div>
       </div>
-    </>
+      <div className="geco-contacts-body">
+        <div className="geco-contacts-options">
+          <GMenuOption option={AdminListContacts} />
+          <GMenuOption option={GroupsContacts} />
+        </div>
+      </div>
+    </div>
   );
 };
