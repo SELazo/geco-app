08c9c4105963dffe92d58cd9c8f68cfefa3c9d04
NelsonLoto <nelson_javier_loto@hotmail.com>
2025-10-15 19:21:53 -0300

    Update sw.js

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.m142l0a1j0g"
+    "revision": "0.qb6g8m99g3o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
